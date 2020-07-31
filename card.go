21caf9e61b774d54208738f1de0ab44ff17e857d
paspartuuu <danworkmail@yandex.ru>
2020-07-31 15:40:27 +0300

    working package

--- card.go
+++ card.go
@@ -10,52 +10,54 @@ type Card struct {
 	Name string
 }
 
-type Deck interface {
-	Shuffle()
-}
+type Deck []Card
 
-type Cards []Card
-
-func New() []Card {
+func New(n int) Deck {
 	var deck []Card
 	var card Card
-	for i := 1; i < 5; i++ {
-		switch i {
-		case 1:
-			card.Suit = "spade"
-		case 2:
-			card.Suit = "diamond"
-		case 3:
-			card.Suit = "club"
-		case 4:
-			card.Suit = "heart"
-		}
-		for g := 1; g < 14; g++ {
-			switch g {
+	for k := 0; k < n; k++ {
+		for i := 1; i < 5; i++ {
+			switch i {
 			case 1:
-				card.Name = "A"
-			case 11:
-				card.Name = "J"
-			case 12:
-				card.Name = "Q"
-			case 13:
-				card.Name = "K"
-			default:
-				card.Name = strconv.Itoa(g)
+				card.Suit = "spade"
+			case 2:
+				card.Suit = "diamond"
+			case 3:
+				card.Suit = "club"
+			case 4:
+				card.Suit = "heart"
 			}
-			deck = append(deck, card)
+			for g := 1; g < 14; g++ {
+				switch g {
+				case 1:
+					card.Name = "A"
+				case 11:
+					card.Name = "J"
+				case 12:
+					card.Name = "Q"
+				case 13:
+					card.Name = "K"
+				default:
+					card.Name = strconv.Itoa(g)
+				}
+				deck = append(deck, card)
+			}
 		}
 	}
 	return deck
 }
 
-func (deck *Cards) Shuffle() {
-	var card Card
-	l := len(*deck)
-	for i := 0; i < l-1; i++ {
-		g := i + 1 + rand.Intn(l-i)
-		card = (*deck)[i]
-		(*deck)[i] = (*deck)[g]
-		(*deck)[g] = card
+func (deck *Deck) Shuffle() {
+	cards := *deck
+	p := rand.Perm(len(*deck))
+	for i, v := range p {
+		(*deck)[i] = cards[v]
+	}
+}
+
+func (hand *Deck) TakeCard(deck *Deck, n int) {
+	for i := 0; i < n; i++ {
+		*hand = append(*hand, (*deck)[len(*deck)-1])
+		*deck = (*deck)[:len(*deck)-1]
 	}
 }
